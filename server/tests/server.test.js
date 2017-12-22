3242467d8770c3428343368c159f66cf3e66b29c
Rishabh Budhiraja <rishabh.budhiraja@gmail.com>
2017-12-22 17:17:16 +0530

    GET /todos test case

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,8 +4,12 @@ const expect = require('expect');
 var {Todo} = require('./../models/todo');
 var {app} = require("./../server");
 
+const text1 = [{text: "Test1 todo"}, {text: "test2 todo"}];
+
 beforeEach((done)=>{
-    Todo.remove({}).then(()=>done());
+    Todo.remove({}).then(()=>{
+        Todo.insertMany(text1).then(()=>done());
+});
 });
 
 describe('POST /todos', ()=> {
@@ -24,7 +28,7 @@ describe('POST /todos', ()=> {
                if(err){
                    return done(err)
                }
-              Todo.find().then((obj)=>{
+              Todo.find({text}).then((obj)=>{
                    expect(obj.length).toBe(1);
                    expect(obj[0].text).toBe(text);
                    done();
@@ -45,9 +49,20 @@ describe('POST /todos', ()=> {
                return done(err);
            }
            Todo.find().then((obj)=>{
-               expect(obj.length).toBe(0);
+               expect(obj.length).toBe(2);
                done();
    }).catch((e)=>done(e));
    });
    });
+});
+
+describe('GET /todo tests', ()=>{
+    it('should get all todos', (done)=>{
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res)=>{
+                expect(res.body.todos.length).toBe(2);
+        }).end(done);
+});
 });
