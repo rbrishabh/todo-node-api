d75d99f3c94f8c0b9e60a38e8b6961ab28a042d0
Rishabh Budhiraja <rishabh.budhiraja@gmail.com>
2017-12-25 14:12:25 +0530

    Effort to deploy app on heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-var link = 'mongodb://rishabh:rishabh@ds163826.mlab.com:63826/todo-app' || 'mongodb://localhost:27017/ToDoApp'
-mongoose.connect(link);
+
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
+
