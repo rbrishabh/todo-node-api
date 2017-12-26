54483a9e3daa1bd0071c2b1e5e5722beb4d8fc60
Rishabh Budhiraja <rishabh.budhiraja@gmail.com>
2017-12-26 14:26:50 +0530

    changed route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp',(err)=>{
+    console.log('could not connect to mongoose database');
+});
 
 module.exports = {mongoose};
 
