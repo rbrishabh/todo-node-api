5babcdc043bb9756a5f1f1d9d491b36a27e5040c
Rishabh Budhiraja <rishabh.budhiraja@gmail.com>
2017-12-26 14:30:46 +0530

    changed route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,9 +2,10 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
+var uri = 'mongodb://rishabh:rishabh@todoapp-shard-00-00-y3yok.mongodb.net:27017,todoapp-shard-00-01-y3yok.mongodb.net:27017,todoapp-shard-00-02-y3yok.mongodb.net:27017/test?ssl=true&replicaSet=TodoApp-shard-0&authSource=admin'
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp',(err)=>{
-    console.log('could not connect to mongoose database');
+mongoose.connect(uri || 'mongodb://localhost:27017/TodoApp',(err)=>{
+    console.log('could not connect to mongoose database', err);
 });
 
 module.exports = {mongoose};
